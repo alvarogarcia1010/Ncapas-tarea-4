0fb87b83ec930193f6b4c6aa0d805b42f4196c0e
alvarogarcia1010 <00111317@uca.edu.sv>
2020-05-03 12:44:55 -0600

    Tarea terminada

--- src/main/java/com/uca/capas/tarea4/domain/Product.java
+++ src/main/java/com/uca/capas/tarea4/domain/Product.java
@@ -1,28 +1,40 @@
 package com.uca.capas.tarea4.domain;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Product {
 	
-	Integer code;
+	@Size(min=12, max=12, message = "El campo Codigo debe tener una longitud de 12 digitos.")
+	@Digits(fraction = 0, integer = 12, message ="El campo Codigo debe ser un numero entero")
+	String code;
 	
-	@Size(min=1, max=100, message = "El campo Nombre debe tener una longitud entre 1 y 100 caracteres.")
+	@NotEmpty(message = "El campo Nombre no puede estar vacio.")
+	@Size(max=100, message = "El campo Nombre debe tener una longitud maxima de 100 caracteres.")
 	String name;
 	
-	@Size(min=1, max=100, message = "El campo Marca debe tener una longitud entre 1 y 100 caracteres.")
+	@NotEmpty(message = "El campo Marca no puede estar vacio.")
+	@Size(max=100, message = "El campo Marca debe tener una longitud maxima de 100 caracteres.")
 	String brand;
 	
-	@Size(min=1, max=500, message = "El campo Descripcion debe tener una longitud entre 1 y 500 caracteres.")
+	@NotEmpty(message = "El campo Descripcion no puede estar vacio.")
+	@Size(max=500, message = "El campo Descripcion debe tener una longitud maxima de 500 caracteres.")
 	String remark;
 	
-	Integer inventory;
+	@Digits(fraction = 0, integer = 10, message ="El campo Codigo debe ser un numero entero")
+	String inventory;
+	
+	@NotEmpty(message = "El campo Fecha de ingreso no puede estar vacio.")
+	@Pattern(regexp = "^(0?[1-9]|[12][0-9]|3[01])[\\/](0?[1-9]|1[012])[\\/]\\d{4}$", message="El formato del campo Fecha debe ser dd/mm/aaaa")
 	String date;
 	
-	public Integer getCode() {
+	public String getCode() {
 		return code;
 	}
 	
-	public void setCode(Integer code) {
+	public void setCode(String code) {
 		this.code = code;
 	}
 	
@@ -50,11 +62,11 @@ public class Product {
 		this.remark = remark;
 	}
 	
-	public Integer getInventory() {
+	public String getInventory() {
 		return inventory;
 	}
 	
-	public void setInventory(Integer inventory) {
+	public void setInventory(String inventory) {
 		this.inventory = inventory;
 	}
 	
